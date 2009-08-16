587273957b91dd0daedebed05354b09de8257927
Luke Closs <lukec@perhaps.lan>
2009-08-15 23:49:57 -0700

    word game

--- static/game.js
+++ static/game.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
     var my_id = Math.random();
 
-    $("#gameboard").click( function() {
-        if ($(this).hasClass('black')) {
-            $(this).removeClass('black');
-            $(this).addClass('white');
-            jQuery.get('/game/update', { pixel: "on", id: my_id});
-        }
-        else {
-            $(this).removeClass('white');
-            $(this).addClass('black');
-            jQuery.get('/game/update', { pixel: "off", id: my_id});
-        }
+    $("#my_word").keypress( function(e) {
+        var my_word = $("#my_word").attr('value');
+        console.log(my_word);
+        jQuery.get('/game/update', { word: my_word, id: my_id});
     });
 });
 
